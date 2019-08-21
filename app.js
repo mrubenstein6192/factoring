139824a97eb76d5c348b1a7c2f971eb9a63d9cd8
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-08-21 15:22:19 -0400

    a > 1 is on its way

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ window.scrollTo(0,0);
 //only for now until the app is more complete
 // $("#welcome").hide();
 $("#prompt").hide();
+$("#promptTwo").hide();
 $("#problem").hide();
 $("#guesses").hide();
+$("#guessesTwo").hide();
 $("#output").hide();
+$("#outputTwo").hide();
 $("#finalCheck").hide();
 
 $(document).on("click", ".start", function() {
@@ -55,15 +58,14 @@ $("#a1").on("click", function() {
   $("#guesses").show();
   $("#first").focus();
   
-  $(document).ready(function(){
+  // autofocus for each input
+    $(document).ready(function(){
     $('input').keyup(function(){
         if(this.value.length==$(this).attr("maxlength")){
-            
-          // $('input').css("visibility", "hidden" )
             $(this).next().focus();
         }
     });
-});
+    });
 
   $("#enterAnswer").on("click", function(event) {
     event.preventDefault();
@@ -106,14 +108,13 @@ $("#a1").on("click", function() {
       secondChoiceString = secondChoice
     }
 
-    
-
     $("#chose").html(firstChoice + " and " + secondChoice);
     var multiplied = firstChoice * secondChoice
     $("#multiplied").html("c = " + firstChoiceString + " x " + secondChoiceString + " = " + multiplied);
     var sum = firstChoice + secondChoice;
     $("#sum").html("b = " + firstChoiceString + " + " + secondChoiceString + " = " + sum);
-
+    
+    //handle html for b
     if (sum > 0) {
       if (sum == 1) {
         sumTwo = " "
@@ -134,9 +135,7 @@ $("#a1").on("click", function() {
     sumTwo = 0;
   }
 
-  
-  console.log(sumTwo);
-
+    //handle html for c
     if (multiplied > 0) {
       product = "+ " + multiplied
     }
@@ -160,23 +159,21 @@ $("#a1").on("click", function() {
       var secondChoiceFinal = " - " + secondChoicePos;
     }
 
+    //html for final expression
     if (sumTwo == 0) {
       $("#answer").html("Your answer of <mark>(x " + firstChoiceFinal + ")(x " + secondChoiceFinal + ")</mark> would FOIL back to the following quadratic expression: <mark>x<sup>2</sup> " + " - " + Math.abs(multiplied) + "</mark>. <br>Is that what you started with?");
       $("#question").html("<mark>" + a1Array[randomIndex] + "</mark>")
-      
     }
     else {
-
     $("#answer").html("Your answer of  <mark>(x " + firstChoiceFinal + ")(x " + secondChoiceFinal + ")</mark> would FOIL back to the following quadratic expression: <mark>x<sup>2</sup> " + sumTwo + "x " + product + "</mark>.  <br>Is that what you started with?");
-
     $("#question").html("<mark>" + a1Array[randomIndex] + "</mark>")
     }
-
+    //correct button
     $("#clickMeTwo").on("click", function() {
       location.reload();
       start();
     })
-
+    //incorrect button
     $("#notReady").on("click", function() {
       $("#first").val("");
       $("#second").val("");
@@ -188,11 +185,78 @@ $("#a1").on("click", function() {
 })
 
 $("#aNot1").on("click", function() {
-  console.log(ranInd);
+  $("#promptTwo").show();
  $("#question").html(secondArray[ranInd]);
- $("#prompt").html("override");
- $("#guesses").html("override");
+ $("#buttonChoices").hide();
+ $("#guessesTwo").show();
+  $("#firstAns").focus();
+ 
+// just the focus function
+  $(document).ready(function(){
+    $('input').keyup(function(){
+        if(this.value.length==$(this).attr("maxlength")){
+            $(this).next().focus();
+        }
+    });
+    });
+
+
+    $("#enterAnswerTwo").on("click", function(event) {
+      event.preventDefault();
+       $("#outputTwo").show();
+       $("#finalCheck").show();
+
+       var firNum = $("#firstAns").val();
+       console.log(firNum);
+       var firstOp = $("#secondAns").val();
+       var empArr = [];
+       empArr.push(firstOp);
+       var uno = "1";
+       empArr.push(uno);
+       var newArrStr = empArr.toString().replace(",", "");
+       var newArrInteger = parseInt(newArrStr, 10);
+       var secNum = $("#thirdAns").val();
+       var secChoice = newArrInteger * secNum;
+       console.log(secChoice);
+
+       var thirdNum = $("#fourthAns").val();
+       console.log(thirdNum);
+       var secOp = $("#fifthAns").val();
+       var empArrTwo = [];
+       empArrTwo.push(secOp);
+       var unoDos = "1";
+       empArrTwo.push(unoDos);
+       var newArrStrTwo = empArrTwo.toString().replace(",", "");
+       var newArrIntegerTwo = parseInt(newArrStrTwo, 10);
+       var fourthNum = $("#sixthAns").val();
+       var fourthChoice = newArrIntegerTwo * fourthNum;
+       console.log(fourthChoice);
+
+       if (secChoice < 0) {
+         var secChoiceString = "(" + secChoice + ")";
+       } 
+       else {
+         secChoiceString = secChoice
+       }
+
+       if (fourthChoice < 0) {
+         var fourthChoiceString = "(" + fourthChoice + ")";
+       }
+       else {
+         fourthChoiceString = fourthChoice;
+       }
+
+       $("#choseTwo").html("First Number: " + firNum + "<br>Second Number: " + secChoice + "<br>Third Number: " + thirdNum + "<br>Fourth Number: " + fourthChoice);
+
+       var multToA = firNum * thirdNum
+       $("#multToA").html("First Number: " + firNum + "<br>Third Number: " + thirdNum + "<br>a = " + firNum + " x " + thirdNum + " = " + multToA)
+    })
+
+//end of aNot1 section
 })
 
+//end start function
 }
+
+//end everything
 })
